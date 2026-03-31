39274f164b80b2f76b954aa2f51ed3dff2f8179a
Annadsantos <anna.dsantos@sptech.school>
2026-03-31 20:48:50 -0300

    atualizacao do BD - adicionando telefone

--- script.sql
+++ script.sql
@@ -64,6 +64,7 @@ nome VARCHAR(100) NOT NULL,
 email VARCHAR(100) NOT NULL UNIQUE,
 senha VARCHAR(255) NOT NULL,
 cpf CHAR(11) NOT NULL UNIQUE,
+telefone VARCHAR(20) UNIQUE,
 fkFabricante INT,
 FOREIGN KEY (fkFabricante) 
     REFERENCES fabricante(idFabricante),
